20910847268ea83983d0641952602f6257ec6496
e310905-saimia <jouni.k.kononen@gmail.com>
2019-09-18 15:03:35 +0300

    FactorialExample Created

---
+++ FactorialExample/FactorialExample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FactorialExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kertoman laskenta");
+        }
+    }
+}
