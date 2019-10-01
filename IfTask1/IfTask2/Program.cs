d4c67e5da3b9fd6a13f68826910e66543d129edd
e310905-saimia <jouni.k.kononen@gmail.com>
2019-10-01 11:14:12 +0300

    IfTask2 Ready

--- IfTask1/IfTask2/Program.cs
+++ IfTask1/IfTask2/Program.cs
@@ -6,7 +6,19 @@ namespace IfTask2
     {
         static void Main(string[] args)
         {
+            int userInputNumber = 0;
             Console.WriteLine("Ohjelma selvittää onko luku pariton vai parillinen!");
+            Console.Write("Syötä kokonaisluku: ");
+            userInputNumber = int.Parse(Console.ReadLine());
+
+            if (userInputNumber % 2 == 0)
+            {
+                Console.WriteLine($"Numero {userInputNumber} on parillinen");                
+            }
+            else
+            {
+                Console.WriteLine($"Numero {userInputNumber} on pariton");
+            }
         }
     }
 }
