fef104ce9b6a8a1acfd9394f9a0b4f1425bccbe4
e310905-saimia <jouni.k.kononen@gmail.com>
2019-10-01 08:58:51 +0300

    IfTask 2 Created

---
+++ IfTask1/IfTask2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IfTask2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma selvittää onko luku pariton vai parillinen!");
+        }
+    }
+}
