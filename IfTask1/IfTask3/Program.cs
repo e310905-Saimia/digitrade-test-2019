d209d4d48f801e6523a9c1d74b444a28590e770f
e310905-saimia <jouni.k.kononen@gmail.com>
2019-10-01 11:15:43 +0300

    IfTask3 Version 1 Ready

---
+++ IfTask1/IfTask3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace IfTask3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma tulostaa positiivinen, negatiivinen tai nolla sekä tarkastaa onko luku parillinen tai pariton.");
+            Console.Write("Syötä luku: ");
+            
+            int userInputNumber = int.Parse(Console.ReadLine());
+            if (userInputNumber < 0)
+            {
+                Console.Write($"Numero {userInputNumber} on negatiivinen, ");
+            }
+            else if (userInputNumber > 0)
+            {
+                Console.Write($"Numero {userInputNumber} on positiivinen, ");
+            }
+            else
+            {
+                Console.Write($"Numero {userInputNumber} on nolla, ");
+            }
+
+
+            if (userInputNumber % 2 == 0)
+            {
+                Console.WriteLine("se on parillinen");
+            }
+            else
+            {
+                Console.WriteLine("se on pariton");
+            }
+        }
+    }
+}
---
+++ IfTask1/IfTask3Vesion2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace IfTask3Vesion2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma tulostaa positiivinen, negatiivinen tai nolla sekä tarkastaa onko luku parillinen tai pariton.");
+            Console.Write("Syötä luku: ");
+            string isSignNegativeOrPositive = "Positiivinen";
+            string isEvenOdd = "Parillinen";
+            int userInputNumber = int.Parse(Console.ReadLine());
+
+            if(userInputNumber==0)
+                Console.WriteLine($"Numero {userInputNumber} on nollaluku");
+            else if (userInputNumber < 0)
+            {
+                isSignNegativeOrPositive = "Negatiivinen";
+            }
+            else
+            {
+                isSignNegativeOrPositive = "Positiivinen";
+            }
+
+            if (userInputNumber % 2 == 0)
+            {
+                isEvenOdd = "Parillinen";
+            }
+            else
+            {
+                isEvenOdd = "Pariton";
+            }
+
+            Console.WriteLine($"Numero {userInputNumber} on {isSignNegativeOrPositive} ja se on {isEvenOdd}");      
+
+        }
+    }
+}
