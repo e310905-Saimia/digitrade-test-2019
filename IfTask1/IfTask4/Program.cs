7483fbb44b1d5aca8a6d3213cc58a46c03e59235
e310905-saimia <jouni.k.kononen@gmail.com>
2019-10-03 08:55:15 +0300

    IFTask4 Variables created

--- IfTask1/IfTask4/Program.cs
+++ IfTask1/IfTask4/Program.cs
@@ -6,7 +6,26 @@ namespace IfTask4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Kolmen luvun lajittelu!");
+            // Tietojen kysely
+            Console.Write("Syötä 1. luku: ");
+            int x = int.Parse(Console.ReadLine());
+
+            Console.Write("Syötä 2. luku: ");
+            int y = int.Parse(Console.ReadLine());
+
+            Console.Write("Syötä 3. luku: ");
+            int z = int.Parse(Console.ReadLine());
+
+            // Logiikka, jolla ratkaistaan ongelma
+            if(x < y)
+            {
+
+            }
+
+            // Tulostetaan vastaus
+
+
         }
     }
 }
