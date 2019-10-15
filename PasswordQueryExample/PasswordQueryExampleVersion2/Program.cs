711d9778faaa1536523b4cb36c861504f34d309e
e310905-saimia <jouni.k.kononen@gmail.com>
2019-10-15 10:53:41 +0300

    PasswordQueryExample Version 2 done This example use for loop

---
+++ PasswordQueryExample/PasswordQueryExampleVersion2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace PasswordQueryExampleVersion2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string password = "qwerty1234";
+            Console.WriteLine("Salasanan kysely For-silmukalla");
+
+            for(int i = 0; i < 3; i++)
+            {
+                Console.Write($"{i+1}. Syötä salasana: ");
+                string userInput = Console.ReadLine();
+
+                if (userInput == password)
+                {
+                    Console.WriteLine("Salasana OIKEIN!");
+                    return;
+                }
+                else
+                {
+                    Console.WriteLine("Väärin meni! Yritä uudelleen");
+                }
+            }
+
+            Console.WriteLine("Tili LUKITTU");
+        }
+    }
+}
