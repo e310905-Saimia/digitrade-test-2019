874f69e0c0b8a1884599a8bc7a8334e3486c9ab1
e310905-saimia <jouni.k.kononen@gmail.com>
2019-10-15 10:33:10 +0300

    PasswordQueryExample versio 1

---
+++ PasswordQueryExample/PasswordQueryExample/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace PasswordQueryExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string password = "qwerty1234";
+            int attemptCounter = 0;
+
+            Console.WriteLine("Syötä salasana.");
+            string userInput = Console.ReadLine();
+
+            while (userInput != password)
+            {
+                attemptCounter++;
+                if(attemptCounter>=3)
+                {
+                    Console.WriteLine("Tili on LUKITTU!");
+                    return;
+                }
+                Console.Write("Väärin! Yritä uudelleen: ");
+                userInput = Console.ReadLine();
+            }
+
+            Console.WriteLine("Salasana OIKEIN!");
+
+        }
+    }
+}
