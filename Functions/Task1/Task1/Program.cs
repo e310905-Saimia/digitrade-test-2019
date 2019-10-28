8942051fc9387a07f80dffbfd1f9aba1d782e3cf
e310905-saimia <jouni.k.kononen@gmail.com>
2019-10-28 12:49:53 +0200

    Function task 1 Created and Done

---
+++ Functions/Task1/Task1/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma tulostaa syötetyn numeron mukaisen määrän tähtiä.");
+            Console.Write("Syötä kokonaisluku: ");
+            int userInput = int.Parse(Console.ReadLine());
+            if (IsValidNumber(userInput))
+            {
+                string strStars = GenerateAmountOfStars(userInput);
+                Console.WriteLine(strStars);
+            }
+            else
+            {
+                Console.WriteLine($"Numero {userInput} ei ole sallittu luku!");
+            }
+            
+        }
+
+        static bool IsValidNumber(int userInput)
+        {
+            return userInput > 0;
+
+            /*
+            if (userInput > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+            */
+        }
+
+
+        /// <summary>
+        /// Ohjelma generoi parametrin määrän tähtiä
+        /// </summary>
+        /// <param name="starCounter"></param>
+        /// <returns></returns>
+        static string GenerateAmountOfStars(int starCounter)
+        {
+            string stars = "";
+            for(int i = 0; i < starCounter; i++)
+            {
+                stars = stars + "*";
+                //stars += "*";
+            }
+            return stars;
+        }
+    }
+}
