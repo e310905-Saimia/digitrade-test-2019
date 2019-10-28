f1d88bfeb5a48422ea04905a397d571c62f7c53b
e310905-saimia <jouni.k.kononen@gmail.com>
2019-10-28 13:31:37 +0200

    Loop task 2 Created and Done

---
+++ LoopTasks/LoopTask2/LoopTask2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace LoopTask2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma laskee N ensimmäistä lukua yhteen.");
+            Console.Write("Syötä luku: ");
+            int userInput = int.Parse(Console.ReadLine());
+            int sumOfNumbers = 0;
+
+            if (userInput < 0)
+            {
+                Console.WriteLine("Virheellinen syöte");
+            }
+            else
+            {
+                // Ohjelman logiikka, silmukka
+                for (int i = 0; i < userInput; i++)
+                {
+                    sumOfNumbers = sumOfNumbers + (i + 1);
+                }
+
+                Console.WriteLine($"Vastaus: {sumOfNumbers}");
+            }
+
+        }
+    }
+}
