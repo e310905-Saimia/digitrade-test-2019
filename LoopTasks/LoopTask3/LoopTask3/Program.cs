b04673758cac52322e5cf35778ad24295b76e90c
e310905-saimia <jouni.k.kononen@gmail.com>
2019-10-30 11:15:54 +0200

    LoopTask3 Done by using for loop

--- LoopTasks/LoopTask3/LoopTask3/Program.cs
+++ LoopTasks/LoopTask3/LoopTask3/Program.cs
@@ -7,7 +7,26 @@ namespace LoopTask3
         static void Main(string[] args)
         {
             Console.WriteLine("Ohjelma laskee N:n ensimmäisen parittoman ja parillisen lukujen summan.");
+            Console.Write("Syötä luku: ");
+            int userInput = int.Parse(Console.ReadLine());
+            int evenSum = 0;
+            int oddSum = 0;
+            
 
+            for (int i = 0; i <= userInput; i++)
+            {
+                
+                if (i % 2 == 0)
+                {
+                    evenSum = evenSum + i;
+                }
+                else
+                {
+                    oddSum = oddSum + i;
+                }
+            }
+            Console.WriteLine($"Parillisten summa = {evenSum}\n" +
+                $"Parittoimien summa = {oddSum}");
         }
     }
 }
