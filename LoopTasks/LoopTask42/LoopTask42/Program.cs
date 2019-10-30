71185ce66755732413e7b45c4fdd821faf266859
e310905-saimia <jouni.k.kononen@gmail.com>
2019-10-30 11:50:01 +0200

    Loop Task 4.2 done

---
+++ LoopTasks/LoopTask42/LoopTask42/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace LoopTask42
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma tulostaa 1-9 kertotaulun");
+            for(int i = 1; i < 10; i++)
+            {
+                for(int j = 1; j < 10; j++)
+                {
+                    Console.WriteLine($"{i} x {j} = {i*j}");
+                }
+            }
+        }
+    }
+}
