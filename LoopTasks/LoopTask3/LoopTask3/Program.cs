e36fc3b522c657a3d278f1f21f7aecae587bed21
e310905-saimia <jouni.k.kononen@gmail.com>
2019-10-30 10:32:21 +0200

    LoopTask 3 Created

---
+++ LoopTasks/LoopTask3/LoopTask3/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace LoopTask3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma laskee N:n ensimmäisen parittoman ja parillisen lukujen summan.");
+
+        }
+    }
+}
