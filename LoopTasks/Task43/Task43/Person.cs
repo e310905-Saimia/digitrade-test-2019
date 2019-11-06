b14f089494e01732c7625bbaa65469638421d890
e310905-saimia <jouni.k.kononen@gmail.com>
2019-11-06 10:56:55 +0200

    LoopTask4.4 created and done

---
+++ LoopTasks/Task43/Task43/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task43
+{
+    class Person
+    {
+        public string firstName;
+        public string lastName;
+
+        string GetName()
+        {
+            return $"{firstName} - {lastName}";
+        }
+
+        public void PrintName()
+        {
+            Console.WriteLine(GetName());
+        }
+    }
+}
---
+++ LoopTasks/Task43/Task43/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Task43
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Satunnaislukugeneraattorin harjoittelua harjoitus 4.3");
+   
+            // Satunnaislukuolion luonti
+            Random rnd = new Random();
+
+            #region "Kaksi sisäkkäistä silmukkaa, jotka tulostavat riville satunnailukuja"
+            // Silmukka, joka tulostaa rivit
+            for (int r = 0; r < 4; r++)
+            {
+                Console.Write($"Rivi {r+1}: ");
+
+                //Tulostaa satunnaisluvut riville
+                for (int i = 0; i < 5; i++)
+                {
+                    if (i < 4)
+                    {
+                        Console.Write($"{rnd.Next(0, 51)}, ");
+                    }
+                    else
+                    {
+                        Console.WriteLine(rnd.Next(0, 51));                       
+                    }
+                }
+                
+            }
+            #endregion
+
+
+        }
+    }
+}
