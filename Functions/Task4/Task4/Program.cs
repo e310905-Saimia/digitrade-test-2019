aaed73f98a17c1f7d44d6b1d72d8ea10b77b1b4b
e310905-saimia <jouni.k.kononen@gmail.com>
2019-11-26 09:21:30 +0200

    resultMessage done and printed in main program

--- Functions/Task4/Task4/Program.cs
+++ Functions/Task4/Task4/Program.cs
@@ -6,7 +6,40 @@ namespace Task4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Syötä 10 positiivista kokonaislukua. Ohjelma palauttaa niistä suurimman ja kertoo monentenako se syötettiin");
+            Console.WriteLine("Ohjelma palauttaa syötetyistä luvuista suurimman ja kertoo monentenako se syötettiin");
+            string resultMessage = MaxNumberCalculator();
+            Console.WriteLine($"Syötit seuraavat luvut:\n {resultMessage}");
+        }
+
+        static string MaxNumberCalculator()
+        {
+            int maxNumber=0;
+            int sequenceOfNumber;
+            string message = string.Empty;
+
+            Console.WriteLine("Syötä 10 kokonaislukua");
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write($"{i+1}. ");
+                string userInput = Console.ReadLine();
+                bool isNumber = int.TryParse(userInput, out int userValue);
+
+                if (isNumber)
+                {
+                    message = $"{message}  {userValue}";
+                    if (userValue > maxNumber)
+                    {
+                        maxNumber = userValue;
+                        sequenceOfNumber = i + 1;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Väärä syöte, syötä luku");
+                    i--;
+                }
+            }
+            return message;
         }
     }
 }
