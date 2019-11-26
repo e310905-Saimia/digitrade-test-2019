0bc0a6444ef0fe4007ae2483d227dc827d023acf
e310905-saimia <jouni.k.kononen@gmail.com>
2019-11-26 08:48:04 +0200

    Task4 Created

---
+++ Functions/Task4/Task4/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Syötä 10 positiivista kokonaislukua. Ohjelma palauttaa niistä suurimman ja kertoo monentenako se syötettiin");
+        }
+    }
+}
