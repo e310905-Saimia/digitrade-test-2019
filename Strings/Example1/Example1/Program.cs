cf1b2e6e5145ceb091d0914aca4f778fb1a56f71
e310905-saimia <jouni.k.kononen@gmail.com>
2019-11-26 10:32:32 +0200

    Print string values one by one

--- Strings/Example1/Example1/Program.cs
+++ Strings/Example1/Example1/Program.cs
@@ -6,7 +6,16 @@ namespace Example1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Merkkijononkäsittely esimerkki");
+            string message = "Hello World!";
+
+            // Tulostetaan message -muuttujen merkit yksitellen näytölle
+            Console.WriteLine($"{message} on {message.Length} merkkiä.");
+
+            for (int i = 0; i < message.Length; i++)
+            {
+                Console.WriteLine(message[i]);
+            }
         }
     }
 }
