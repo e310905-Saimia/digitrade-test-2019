19c3f483e6d63a1c4a8a35589e6f26bdc3aa18ee
e310905-saimia <jouni.k.kononen@gmail.com>
2019-12-12 12:55:24 +0200

    Example done

--- Strings/Example1/Example1/Program.cs
+++ Strings/Example1/Example1/Program.cs
@@ -7,14 +7,40 @@ namespace Example1
         static void Main(string[] args)
         {
             Console.WriteLine("Merkkijononkäsittely esimerkki");
-            string message = "Hello World!";
+            string message = "  Hello World!  ";
 
             // Tulostetaan message -muuttujen merkit yksitellen näytölle
             Console.WriteLine($"{message} on {message.Length} merkkiä.");
+            Console.WriteLine("----------");
 
-            for (int i = 0; i < message.Length; i++)
+            PrintMessage(message);
+            Console.WriteLine("----------");
+
+            //Poistetaan välilyönnit alusta ja lopusta
+            string tempMessage = message.Trim();
+            PrintMessage(tempMessage);
+            Console.WriteLine("----------");
+
+            tempMessage = tempMessage.ToUpper();
+            PrintMessage(tempMessage);
+            Console.WriteLine("----------");
+
+            // Etsitään merkin kohta merkkijonosta
+            int position = tempMessage.IndexOf(' ');
+            Console.WriteLine($"Välilyönti löytyy kohdasta {position}");
+            //for (int i = 0; i < message.Length; i++)
+            //{
+            //    Console.WriteLine(message[i]);
+            //}
+
+
+        }
+
+        static void PrintMessage(string s)
+        {
+            for (int i = 0; i < s.Length; i++)
             {
-                Console.WriteLine(message[i]);
+                Console.WriteLine(s[i]);
             }
         }
     }
