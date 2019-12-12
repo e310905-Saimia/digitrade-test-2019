002451c9630d7444473ac41991c2918120cbed36
e310905-saimia <jouni.k.kononen@gmail.com>
2019-12-12 12:56:09 +0200

    SosialSecurityNumber project created

---
+++ Strings/SosialSecurityNumber/SosialSecurityNumber/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SosialSecurityNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma tarkastaa Hetun oikeellisuuden.");
+            
+        }
+    }
+}
