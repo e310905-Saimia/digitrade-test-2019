4baae9f88caa53d7b16368b195351d6a909a6a7a
e310905-saimia <jouni.k.kononen@gmail.com>
2019-12-12 13:37:59 +0200

    SosialSecurityNumber - Input Parser done - IsValid Function 1. version

--- Strings/SosialSecurityNumber/SosialSecurityNumber/Program.cs
+++ Strings/SosialSecurityNumber/SosialSecurityNumber/Program.cs
@@ -7,7 +7,45 @@ namespace SosialSecurityNumber
         static void Main(string[] args)
         {
             Console.WriteLine("Ohjelma tarkastaa Hetun oikeellisuuden.");
-            
+            string userInput = "141052-308T";
+            int idNumber =  InputParser(userInput);
+            IsValidID(idNumber);
+
         }
+
+        static int InputParser(string stringParser)
+        {
+            string removed = stringParser.Remove(10, 1);
+            removed = removed.Remove(6,1);
+
+            return int.Parse(removed);
+        }
+
+        static bool IsValidID(int idNumber)
+        {
+            string chkMark = "0123456789ABCDEFHJKLMNPRSTUVWXY";
+
+            int modChecker = idNumber % 31;
+
+            Console.WriteLine(chkMark[modChecker]);
+            return true;
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
