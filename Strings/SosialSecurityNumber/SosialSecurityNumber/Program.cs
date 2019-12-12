8a7ba1c620dbd1fdab1f6f9c2ed7d8521a2f73d8
e310905-saimia <jouni.k.kononen@gmail.com>
2019-12-12 14:02:58 +0200

    SosialSecurityNumber: Added functions GetUserInputChkMark and PrintResult

--- Strings/SosialSecurityNumber/SosialSecurityNumber/Program.cs
+++ Strings/SosialSecurityNumber/SosialSecurityNumber/Program.cs
@@ -7,30 +7,47 @@ namespace SosialSecurityNumber
         static void Main(string[] args)
         {
             Console.WriteLine("Ohjelma tarkastaa Hetun oikeellisuuden.");
-            string userInput = "141052-308T";
-            int idNumber =  InputParser(userInput);
-            IsValidID(idNumber);
+            string userInput = "131052-308T";
+            int idNumber = InputParser(userInput);
+            char getLastChar = GetUserInputChkMark(userInput);
+            bool isOK = IsValidID(idNumber, getLastChar);
+            PrintResult(isOK);
+        }
 
+        static char GetUserInputChkMark(string userInput)
+        {
+            return userInput[userInput.Length - 1];
         }
 
         static int InputParser(string stringParser)
         {
             string removed = stringParser.Remove(10, 1);
-            removed = removed.Remove(6,1);
+            removed = removed.Remove(6, 1);
 
             return int.Parse(removed);
         }
 
-        static bool IsValidID(int idNumber)
+        static bool IsValidID(int idNumber, char userInputChkMark)
         {
             string chkMark = "0123456789ABCDEFHJKLMNPRSTUVWXY";
 
             int modChecker = idNumber % 31;
 
-            Console.WriteLine(chkMark[modChecker]);
-            return true;
+            // Vertailu onko käyttäjän syöttämä viimeinen merkki sama kuin laskettu
+            if (chkMark[modChecker] == userInputChkMark)
+                return true;
+            else
+                return false;
 
+            // return chkMark[modChecker] == userInputChkMark;
+        }
 
+        static void PrintResult(bool isValidId)
+        {
+            if (isValidId)
+                Console.WriteLine("Sotu on oikein!");
+            else
+                Console.WriteLine("Sotu on väärin!");
         }
 
 
