b4da0db2b3f6556700170eebffc98e8ec96608b8
e310905-saimia <jouni.k.kononen@gmail.com>
2019-12-13 13:12:25 +0200

    SosialSecurityNumber Function IsValidDate added and done

--- Strings/SosialSecurityNumber/SosialSecurityNumber/Program.cs
+++ Strings/SosialSecurityNumber/SosialSecurityNumber/Program.cs
@@ -7,12 +7,71 @@ namespace SosialSecurityNumber
         static void Main(string[] args)
         {
             Console.WriteLine("Ohjelma tarkastaa Hetun oikeellisuuden.");
-            string userInput = " 131052 - 308 T   ";
-            RemoveSpaces(userInput);
-            int idNumber = InputParser(userInput);
-            char getLastChar = GetUserInputChkMark(userInput);
-            bool isOK = IsValidID(idNumber, getLastChar);
-            PrintResult(isOK);
+            string userInput = " 300252 - 308 T   ";
+
+            userInput = RemoveSpaces(userInput);
+            if (IsValidLenght(userInput))
+            {
+                if (IsValidDate(userInput))
+                {
+                    int idNumber = InputParser(userInput);
+                    char getLastChar = GetUserInputChkMark(userInput);
+                    bool isOK = IsValidID(idNumber, getLastChar);
+                    PrintResult(isOK);
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("Tarkasta hetun oikeellisuus - liikaa merkkejä");
+            }
+        }
+
+        static bool IsValidDate(string userInput)
+        {
+            //131052-308T
+            bool result = false;
+            // 
+            string day = userInput.Substring(0, 2);
+            string month = userInput.Substring(2,2);
+            string year = userInput.Substring(4, 2);
+            string century = userInput.Substring(6, 1);
+
+            if(century == "-")
+            {
+                year = "19" + year;
+            }
+            else if(century=="A")
+            {
+                year = "20" + year;
+            }
+            else
+            {
+                Console.WriteLine("Väärä vuosisata");
+            }
+
+            // Tarkastetaan päivämäärän oikeellisuus try-catch lohkossa
+            try
+            {
+                DateTime birthday = new DateTime(int.Parse(year), int.Parse(month), int.Parse(day));
+                result = true;
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+
+            }
+            return result;
+        }
+        static bool IsValidLenght(string userInput)
+        {
+            return userInput.Length == 11;
+
+            //if (userInput.Length == 11)
+            //    return true;
+            //else
+            //    return false;
+
         }
 
         static string RemoveSpaces(string userInput)
