467565c6b733be852110f433533c3391b9091192
e310905-saimia <jouni.k.kononen@gmail.com>
2019-12-18 10:39:10 +0200

    FileHandlingExample Created

---
+++ Files/FileHandlingExample/FileHandlingExample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FileHandlingExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tiedoston kirjoitus ja luku esimerkki");
+        }
+    }
+}
