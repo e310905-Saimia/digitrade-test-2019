0e9d5d59218b9efd5fde793768a792cf5fdc2c1b
e310905-saimia <jouni.k.kononen@gmail.com>
2019-12-18 11:24:11 +0200

    FileHandlingExample Functions - WriteToFile - done - ReadFile - done

--- Files/FileHandlingExample/FileHandlingExample/Program.cs
+++ Files/FileHandlingExample/FileHandlingExample/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace FileHandlingExample
 {
@@ -6,7 +7,35 @@ namespace FileHandlingExample
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Tiedoston kirjoitus ja luku esimerkki");
+            Console.WriteLine("Tiedoston kirjoitus ja luku esimerkki.");
+            string path = @"MyTest.txt";
+            WriteToFile(path);
+            ReadFile(path);          
+        }// End Main Program
+
+        static void WriteToFile(string path)
+        {
+            DateTime currentTime = DateTime.Now;
+            using (StreamWriter sw = File.AppendText(path))
+            {
+                sw.WriteLine($"{currentTime} ------------");
+                sw.WriteLine("Hello");
+                sw.WriteLine("And");
+                sw.WriteLine("Welcome");
+                sw.WriteLine("Tässä Jounin rivi");
+            }
+        }
+
+        static void ReadFile(string path)
+        {
+            using (StreamReader sr = File.OpenText(path))
+            {
+                string s;
+                while ((s = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(s);
+                }
+            }
         }
     }
 }
